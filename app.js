7e2024eaf7450db7363f1a0fad3f3e0e9ab3f88d
Skye Brown <skye.brownh@gmail.com>
2020-03-02 23:53:26 -0500

    install mongoose; configure and setup mongodb connection

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// db config
+const db = require('./config/keys').mongoUri;
+
+// mongodb connection
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.error(err));
 
 // ejs
 app.use(expressLayouts);
