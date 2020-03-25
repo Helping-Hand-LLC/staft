8fde62f4792662dee2d10d7afa837e786f4889b1
Skye Brown <skye.brownh@gmail.com>
2020-03-25 11:15:48 -0400

    add color theme to tailwind config; remove bootstrap and include tailwind in layout.ejs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   theme: {
+    colors: {
+      primary: '#001634',
+      secondary: '#00b4a6',
+      lightgrey: '#bebcbc',
+      lightergrey: '#f0f0f0',
+      white: '#fdfffc'
+    },
     extend: {},
   },
   variants: {},
