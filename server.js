e08e19755f6cb22d16fed15797cb0fcd56bea14d
Skye Brown <skye.brownh@gmail.com>
2020-04-17 17:22:50 -0400

    mongodb, body parser setup

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const { port, mongoUri } = require('./config/keys');
 
 const app = express();
+
+// mongodb connection
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+  .then(() => console.log('mongodb connected...'))
+  .catch(err => console.error(err));
+
+// body parser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.route('/')
   .get((req, res) => res.send('hello world!'));
 
-app.listen(5000, () => console.log('express server listening on port 5000...'));
+app.listen(port, () => console.log(`express server listening on port ${port}...`));
