64611a8905a22aa37d25d4612dfde1d486fce233
Skye Brown <skye.brownh@gmail.com>
2020-05-18 19:08:24 -0400

    userSchema method to generate auth tokens

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const Profile = require('./Profile');
+const jwt = require('jsonwebtoken');
+const { privateKey } = require('../config/keys');
 
 const userSchema = new mongoose.Schema({
   type: {
@@ -21,6 +24,36 @@ const userSchema = new mongoose.Schema({
     default: Date.now()
   }
 });
+
+userSchema.methods.generateAuthToken = () => {
+  return new Promise((resolve, reject) => {
+    Profile.findOne({ user: this.id })
+      .then(profile => {
+        // set access level in jwt payload
+        let isAdmin = false,
+          isManager = false;
+        if (profile) {
+          isAdmin = profile.isAdmin;
+          isManager = profile.isManager;
+        }
+
+        jwt.sign(
+          {
+            id: this.id,
+            isAdmin,
+            isManager,
+            expiresIn: '2 days'
+          },
+          privateKey,
+          (err, token) => {
+            if (err) reject(err);
+            resolve(token);
+          }
+        );
+      })
+      .catch(err => reject(err));
+  });
+};
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
