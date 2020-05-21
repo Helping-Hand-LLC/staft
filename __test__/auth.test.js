6aecc1ac584d67726a6bc72450fb7f33b8738d7f
Skye Brown <skye.brownh@gmail.com>
2020-05-21 13:20:32 -0400

    test jest framework works; silence npm errors when running > staft@1.0.0 test /Users/skyebrown/Documents/helpinghands/staft > jest || exit 0

---
+++ __test__/auth.test.js
@@ -0,0 +1,7 @@
+const { describe, expect, it } = require('@jest/globals');
+
+describe('Testing Staft API', () => {
+  it('Tests that our testing framework works properly', () => {
+    expect(4).toBe(4);
+  });
+});
