bb1a24150047aeff980440732ef7e73075a9178f
Skye Brown <skye.brownh@gmail.com>
2020-05-21 17:57:45 -0400

    hello world in typescript

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+
+// routes
+app.get('/', (_req: Request, res: Response) => res.send('hello world'));
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,4 @@
+import app from './app';
+
+// start server
+app.listen(5000, () => console.log('express server listening on port 3000...'));
