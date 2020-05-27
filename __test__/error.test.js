91b1d96ff1d963aef97a6c37fe069c0b817262e9
Skye Brown <skye.brownh@gmail.com>
2020-05-27 17:32:11 -0400

    refactor error tests to use 'describe' and 'it'; checkObjectId middleware tests

--- __test__/error.test.js
+++ __test__/error.test.js
@@ -1,14 +1,16 @@
 /* eslint-disable no-undef */
 const { routeError } = require('../utils/error');
 
-test('error utility returns a correctly formatted error object with message', () => {
-  expect(routeError('some error message')).toEqual({
-    errors: [{ msg: 'some error message' }]
+describe('Test error utility', () => {
+  it('error utility returns a correctly formatted error object with message', () => {
+    expect(routeError('some error message')).toEqual({
+      errors: [{ msg: 'some error message' }]
+    });
   });
-});
 
-test('error utility returns a general message if nothing was passed', () => {
-  expect(routeError()).toEqual({
-    errors: [{ msg: 'Route Error' }]
+  it('error utility returns a general message if nothing was passed', () => {
+    expect(routeError()).toEqual({
+      errors: [{ msg: 'Route Error' }]
+    });
   });
 });
