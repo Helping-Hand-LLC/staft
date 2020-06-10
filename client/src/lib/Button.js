b0b1f603f7d8edc86ed9ff78123a1ec3e785b750
Skye Brown <skye.brownh@gmail.com>
2020-06-10 15:42:03 -0400

    index page redesign; library Button fixes

--- client/src/lib/Button.js
+++ client/src/lib/Button.js
@@ -1,22 +1,64 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-function Button() {
-  return (
-    <button class='bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded'>
-      Button
-    </button>
-  );
-}
+import _ from 'lodash';
 
-function Outlined() {
-  return (
-    <button class='bg-transparent hover:bg-blue text-blue-dark font-semibold hover:text-white py-2 px-4 border border-blue hover:border-transparent rounded'>
-      Button
-    </button>
-  );
-}
-
-module.exports = {
-  Button,
-  Outlined
+const defaultProps = {
+  display: 'inline-block',
+  bgColor: 'bg-primary',
+  textColor: 'text-newwhite',
+  borderRadius: 'rounded',
+  fontSize: 'text-base',
+  fontWeight: 'font-normal',
+  textTransform: 'normal-case',
+  padding: 'py-2 px-4'
+  // no default margin
+  // no default flex
 };
+
+const propTypes = {
+  display: PropTypes.string,
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string,
+  borderRadius: PropTypes.string,
+  fontSize: PropTypes.string,
+  fontWeight: PropTypes.string,
+  textTransform: PropTypes.string,
+  margin: PropTypes.string,
+  flex: PropTypes.string
+};
+
+export function Button(props) {
+  return (
+    <button className={Object.values(_.omit(props, ['children'])).join(' ')}>
+      {props.children}
+    </button>
+  );
+}
+
+export function ButtonLink(props) {
+  return (
+    <Link
+      to={props.to}
+      className={Object.values(_.omit(props, ['to', 'children'])).join(' ')}
+    >
+      {props.children}
+    </Link>
+  );
+}
+
+export function Outlined(props) {
+  return (
+    <button className='bg-transparent hover:bg-blue text-blue-dark font-semibold hover:text-white py-2 px-4 border border-blue hover:border-transparent rounded'>
+      Button
+    </button>
+  );
+}
+
+Button.defaultProps = defaultProps;
+Button.propTypes = propTypes;
+ButtonLink.defaultProps = defaultProps;
+ButtonLink.propTypes = propTypes;
+Outlined.defaultProps = defaultProps;
+Outlined.propTypes = propTypes;
--- client/src/pages/Index.js
+++ client/src/pages/Index.js
@@ -1,23 +1,67 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
+import { Button, ButtonLink } from '../lib/Button';
+
+import ManInSuitImg from '../images/hunters-race-MYbhN8KaaEc-unsplash.jpg';
 
 function Index() {
   return (
-    <div className='h-screen flex flex-col justify-between text-center py-8'>
-      <section>
-        <h1 className='uppercase text-2xl font-normal my-4'>Staft</h1>
-        <p className='font-light'>Bringing people together, better</p>
-      </section>
-      <section>
-        <button className='inline-block rounded bg-primary text-newwhite uppercase font-thin text-sm py-2 px-8 my-4'>
-          Create an organization
-        </button>
-        <small className='block'>
-          <Link className='underline' to='/login'>
-            Log in
-          </Link>
-        </small>
-      </section>
+    <div
+      className='h-screen'
+      style={{
+        backgroundImage: `url(${ManInSuitImg})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center'
+      }}
+    >
+      <div
+        className='h-screen flex flex-col justify-between text-center py-8'
+        style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
+      >
+        <section>
+          <h1 className='uppercase text-3xl font-normal my-2 text-newwhite'>
+            Staft
+          </h1>
+          <p className='font-light text-newwhite text-lg italic'>
+            Bringing people together, better.
+          </p>
+        </section>
+        <section className='h-32 w-5/6 mx-auto flex flex-col justify-center'>
+          <Button
+            bgColor='bg-secondary'
+            textTransform='uppercase'
+            fontSize='text-sm'
+            margin='my-4'
+          >
+            Create an organization
+          </Button>
+          <div className='flex justify-between'>
+            <ButtonLink
+              to='/register'
+              bgColor='bg-newwhite'
+              textColor='text-primary'
+              textTransform='uppercase'
+              fontSize='text-sm'
+              flex='flex-1'
+              margin='mr-1'
+            >
+              I'm a Guest
+            </ButtonLink>
+            <ButtonLink
+              to='/login'
+              bgColor='bg-newwhite'
+              textColor='text-primary'
+              textTransform='uppercase'
+              fontSize='text-sm'
+              flex='flex-1'
+              margin='ml-1'
+            >
+              Log In
+            </ButtonLink>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
