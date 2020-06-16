cc2a5f8f502def3622098f20752501aeebc273ee
Skye Brown <skye.brownh@gmail.com>
2020-06-15 23:33:28 -0400

    remove Dashboard components; start Dashboard Drawer

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import Index from './pages/Index';
 import Login from './pages/Login';
 import Register from './pages/Register';
-// import Dashboard from './pages/Dashboard';
+import Dashboard from './pages/Dashboard';
 
 import './App.css';
 
@@ -18,11 +18,10 @@ function App() {
         <Route path='/register'>
           <Register />
         </Route>
-        {/* <Route path='/forgot'>Forgot</Route>
         <Route path='/dashboard'>
           <Dashboard />
-        </Route> */}
-        <Route path='/' exact>
+        </Route>
+        <Route path='/'>
           <Index />
         </Route>
       </Switch>
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,20 +1,121 @@
 import React, { useState } from 'react';
-import { Switch, Route, useRouteMatch, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import {
+  Link,
+  Switch,
+  Route,
+  useRouteMatch,
+  useParams,
+  Redirect
+} from 'react-router-dom';
+import {
+  AppBar,
+  Toolbar,
+  Drawer,
+  Hidden,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 
-import DashboardSidebar from '../components/DashboardSidebar';
-import DashboardMenuItem from '../components/DashboardMenuItem';
+const drawerWidth = 256; // 64 on tailwindcss scale
+
+// function DirectMessages(props) {
+//   return (
+//     <>
+//       <DashboardHeader title='Messages' handleClick={props.handleClick} />
+//       <div>DMs</div>
+//     </>
+//   );
+// }
+
+// function Schedule(props) {
+//   return (
+//     <>
+//       <DashboardHeader title='Schedule' handleClick={props.handleClick} />
+//       {/* tabs */}
+//       <ul className='flex bg-newlightergrey rounded w-11/12 mx-auto p-1'>
+//         <li className='flex-1 mr-2'>{/* TODO: implement me */}</li>
+//         <li className='flex-1 mr-2'>{/* TODO: implement me */}</li>
+//       </ul>
+//       {/* events */}
+//     </>
+//   );
+// }
+
+// function DashboardHeader(props) {
+//   return (
+//     <div className='p-4 flex'>
+//       <button className='inline-block' onClick={props.handleClick}>
+//         {/* <FontAwesomeIcon icon={faBars} /> */}
+//       </button>
+//       <h4 className='flex-1 text-center'>{props.title}</h4>
+//     </div>
+//   );
+// }
+
+// function DashboardSidebar(props) {
+//   return (
+//     <section
+//       className={`relative h-screen w-64 ${
+//         props.isOpen ? 'ml-0' : '-ml-64'
+//       } bg-newlightergrey flex flex-col justify-between duration-500 transition-marginLeft`}
+//     >
+//       <ul className='p-4'>
+//         <li className='sidebar-link'>
+//           {/* <FontAwesomeIcon icon={faCalendarAlt} /> */}
+//           &nbsp;
+//           <Link to={`${props.url}/schedule`}>Schedule</Link>
+//         </li>
+//         <li className='sidebar-link'>
+//           {/* <FontAwesomeIcon icon={faCommentDots} /> */}
+//           &nbsp;
+//           <Link to={`${props.url}/messages`}>Direct Messages</Link>
+//         </li>
+//       </ul>
+//       <div className='bg-primary text-newwhite flex py-2 px-3'>
+//         <img
+//           className='rounded-full mr-3'
+//           src='https://via.placeholder.com/50.png/FFFFFF'
+//           alt='Worker Profile'
+//         />
+//         <div className='flex-1'>
+//           <h6>Last, First</h6>
+//           <small className='text-newlightgrey'>Team Member</small>
+//         </div>
+//         <small className='inline-block'>
+//           <Link to='/logout'>Logout</Link>
+//         </small>
+//       </div>
+//     </section>
+//   );
+// }
+
+// function DashboardMenuItem(props) {
+//   let { menuItem } = useParams();
+
+//   switch (menuItem) {
+//     case 'schedule':
+//       return <Schedule handleClick={props.handleClick} />;
+//     case 'messages':
+//       return <DirectMessages handleClick={props.handleClick} />;
+//   }
+// }
 
 function Dashboard() {
-  // url - parent path
-  // path - relative path matching
-  let { url, path } = useRouteMatch();
+  // path - allows us to build <Route> paths relative to parent path
+  // url - allows us to build relative links
+  let { path, url } = useRouteMatch();
 
   const [isOpen, setIsOpen] = useState(false);
 
-  function handleClick(e) {
+  const handleClick = e => {
     e.preventDefault();
     isOpen ? setIsOpen(false) : setIsOpen(true);
-  }
+  };
 
   return (
     <div className='h-screen flex flex-no-wrap overflow-x-hidden'>
@@ -40,3 +141,25 @@ function Dashboard() {
 }
 
 export default Dashboard;
+
+DirectMessages.propTypes = {
+  handleClick: PropTypes.func.isRequired
+};
+
+Schedule.propTypes = {
+  handleClick: PropTypes.func.isRequired
+};
+
+DashboardHeader.propTypes = {
+  title: PropTypes.string.isRequired,
+  handleClick: PropTypes.func.isRequired
+};
+
+DashboardSidebar.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  url: PropTypes.string.isRequired
+};
+
+DashboardMenuItem.propTypes = {
+  handleClick: PropTypes.func.isRequired
+};
