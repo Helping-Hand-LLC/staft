d36ff4eba894188a76df8e771c35ab262f719d1e
Skye Brown <skye.brownh@gmail.com>
2020-06-19 17:20:56 -0400

    backend Profile page information

--- client/src/components/dashboard/Profile.js
+++ client/src/components/dashboard/Profile.js
@@ -17,7 +17,7 @@ export default function Profile({ handleClick }) {
         secondaryIcon={<EditOutlinedIcon />}
         handleClick={handleClick}
       />
-      <div className='h-screen flex flex-col'>
+      <div>
         <section className='h-24 flex flex-col justify-center items-center'>
           <AccountCircleOutlinedIcon fontSize='large' className='my-2' />
           <h3>Skye Brown</h3>
@@ -25,7 +25,71 @@ export default function Profile({ handleClick }) {
             Joined Staft on February 2020
           </small>
         </section>
-        <section className='flex-1 py-4'>
+        <section className='py-4 text-sm'>
+          {/* type, email, name, address, phone, birthday, gender, ssn, organization, isManager, isAdmin */}
+          <div className='inline-block w-1/2 p-2'>
+            <h6 className='font-medium mb-1'>Name</h6>
+            <p className='font-light text-gray-600'>Skye Brown</p>
+          </div>
+          <div className='inline-block w-1/2 p-2'>
+            <h6 className='font-medium mb-1'>Email</h6>
+            <p className='font-light text-gray-600'>skye.brown@uky.edu</p>
+          </div>
+
+          <div className='inline-block w-1/2 p-2'>
+            <h6 className='font-medium mb-1'>Phone</h6>
+            <p className='font-light text-gray-600'>1 (123) 456-7890</p>
+          </div>
+          <div className='inline-block w-1/2 p-2'>
+            <h6 className='font-medium mb-1'>Birthday</h6>
+            <p className='font-light text-gray-600'>January 1, 2000</p>
+          </div>
+
+          <div className='inline-block w-1/2 p-2'>
+            <h6 className='font-medium mb-1'>Gender</h6>
+            <p className='font-light text-gray-600'>Female</p>
+          </div>
+          <div className='inline-block w-1/2 p-2 pr-4'>
+            <h6 className='font-medium mb-1'>SSN</h6>
+            <p className='font-light text-gray-600 flex justify-between items-center'>
+              ***-**-***
+              <button
+                className='text-xs hover:underline ml-4'
+                style={{ outline: 'none' }}
+              >
+                Show
+              </button>
+            </p>
+          </div>
+
+          <div className='block p-2'>
+            <h6 className='font-medium mb-1'>Address</h6>
+            <p className='font-light text-gray-600'>
+              <span>123 Main St</span>
+              <br />
+              <span>New York City</span>
+              {', '}
+              <span>NY</span>
+              <span>12345</span>
+            </p>
+          </div>
+
+          <div className='border border-gray-400 p-2'>
+            <div className='mb-2'>
+              <h6 className='font-medium'>Organization</h6>
+              <p className='font-light text-gray-600'>Helping Hand LLC</p>
+            </div>
+            <div className='inline-block w-1/2'>
+              <h6 className='font-medium'>Manager</h6>
+              <p className='font-light text-gray-600'>No</p>
+            </div>
+            <div className='inline-block w-1/2'>
+              <h6 className='font-medium'>Administrator</h6>
+              <p className='font-light text-gray-600'>No</p>
+            </div>
+          </div>
+        </section>
+        <section className='py-4'>
           <Link className='block w-full bg-white p-2 text-sm font-light border-t border-b border-gray-400 flex justify-between'>
             Settings
             <KeyboardArrowRightIcon />
