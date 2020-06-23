e0e46029aed00d2f0b1dc80229fe297a837ecfec
Skye Brown <skye.brownh@gmail.com>
2020-06-23 12:16:13 -0400

    Organization Details page

--- client/src/pages/organization/Details.js
+++ client/src/pages/organization/Details.js
@@ -1,15 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from '../../components/Header';
 
 export default function Details() {
+  const [uid, setUid] = useState('');
+  const [accessType, setAccessType] = useState('public');
+
+  const handleUidChange = e => setUid(e.target.value);
+  const handleAccessTypeChange = e => setAccessType(e.target.value);
+
   return (
     <>
-      <Header title='Details' backPath='/dashboard/org/settings' />
+      <Header
+        title='Details'
+        secondaryIcon={
+          <span
+            className='text-teal-500 font-light'
+            style={{ outline: 'none' }}
+          >
+            Done
+          </span>
+        }
+        backPath='/dashboard/org/settings'
+      />
 
-      <div className='h-64 flex justify-center items-center text-gray-600'>
-        Details: Coming Soon...
-      </div>
+      <form className='text-sm'>
+        <label
+          htmlFor='uid'
+          className='block w-full border-t border-b border-gray-400 mb-4'
+        >
+          <input
+            className='w-full h-full placeholder-gray-400 text-teal-500 outline-none p-3'
+            type='text'
+            name='uid'
+            id='uid'
+            placeholder='Organization Name'
+            value={uid}
+            onChange={handleUidChange}
+          />
+        </label>
+        <label
+          htmlFor='access-public'
+          className='flex items-center text-sm font-light'
+        >
+          <input
+            type='radio'
+            name='accessType'
+            id='access-public'
+            className='text-teal-500 mr-2 w-1/5'
+            checked={accessType === 'public'}
+            value='public'
+            onChange={handleAccessTypeChange}
+          />
+          <span className='flex-1 text-left'>Public</span>
+        </label>
+        <br />
+        <label
+          htmlFor='access-private'
+          className='flex items-center text-sm font-light'
+        >
+          <input
+            type='radio'
+            name='accessType'
+            id='access-private'
+            className='text-teal-500 mr-2 w-1/5'
+            checked={accessType === 'private'}
+            value='private'
+            onChange={handleAccessTypeChange}
+          />
+          <span className='flex-1 text-left'>Private (invite only)</span>
+        </label>
+      </form>
     </>
   );
 }
