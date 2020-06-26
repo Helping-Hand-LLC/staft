4048d398caf2fbc388d1d4b8253a9607ee1bbccd
Skye Brown <skye.brownh@gmail.com>
2020-06-25 20:36:29 -0400

    modify Invite to submit member emails

--- client/src/pages/organization/Invite.js
+++ client/src/pages/organization/Invite.js
@@ -1,26 +1,45 @@
 import React, { useState } from 'react';
 import CloseIcon from '@material-ui/icons/Close';
-import SearchIcon from '@material-ui/icons/Search';
+import AlternateEmailIcon from '@material-ui/icons/AlternateEmail';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 import Header from '../../components/Header';
 import { Button } from '../../lib/Button';
 
-import _members from '../../constants/members.json';
-
-function Member({ name }) {
+function Member({ name, handleClick }) {
   return (
-    <li className='p-2 flex items-center border-b border-gray-400 font-light text-sm'>
-      <AccountCircleIcon className='mr-4' />
+    <li className='p-2 flex justify-between items-center border-b border-gray-400 font-light text-sm'>
+      <AccountCircleIcon />
       {name}
+      <button type='button' onClick={handleClick}>
+        <CloseIcon
+          fontSize='small'
+          className='bg-red-500 text-white rounded p-1'
+        />
+      </button>
     </li>
   );
 }
 
 export default function Invite() {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [invitees, setInvitees] = useState([]);
+  const [memberEmail, setMemberEmail] = useState('');
 
-  const handleSearchTermChange = e => setSearchTerm(e.target.value);
+  const handleMemberEmailChange = e => setMemberEmail(e.target.value);
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log('form submitted');
+  };
+  const addInvitee = e => {
+    e.preventDefault();
+
+    // TODO: email validation
+
+    setInvitees([...invitees, memberEmail]);
+    setMemberEmail('');
+  };
+  const removeInvitee = removeIndex =>
+    setInvitees(invitees.filter((invitee, index) => index !== removeIndex));
 
   return (
     <>
@@ -30,35 +49,48 @@ export default function Invite() {
         backPath='/dashboard/org/members'
       />
 
-      <form className='text-sm'>
+      <p className='text-sm p-4 text-gray-700 text-center font-light'>
+        Add members to your organization via email. These members must be a
+        registered Staft user who has completed their profile and is not already
+        assigned to another organization.
+      </p>
+
+      <form className='text-sm' onSubmit={handleSubmit}>
         <div className='w-full flex justify-between items-center bg-gray-300'>
-          <SearchIcon fontSize='small' className='text-gray-500 mx-2' />
+          <AlternateEmailIcon fontSize='small' className='text-gray-500 mx-2' />
           <input
             className='p-2 w-full outline-none text-teal-500 bg-gray-300'
-            type='search'
-            name='org-search'
-            id='org-search'
-            placeholder='Search'
-            value={searchTerm}
-            onChange={handleSearchTermChange}
+            type='email'
+            name='memberEmail'
+            id='memberEmail'
+            placeholder='Member Email'
+            value={memberEmail}
+            onChange={handleMemberEmailChange}
           />
+          <Button
+            fontSize='text-sm'
+            style={{ outline: 'none' }}
+            onClick={addInvitee}
+          >
+            Add
+          </Button>
         </div>
         <h4 className='uppercase text-gray-500 p-2 mt-2 flex justify-between items-center'>
           <span className='h-px bg-gray-400 flex-1 mr-2'></span>
-          Contacts
+          Members
           <span className='h-px bg-gray-400 flex-1 ml-2'></span>
         </h4>
         <ul>
-          {/* TODO: change to checkbox inputs to invite multiple */}
-          {_members
-            .filter(
-              m => m.name.toLowerCase().search(searchTerm.toLowerCase()) !== -1
-            )
-            .map((m, i) => (
-              <Member key={i} name={m.name} />
-            ))}
+          {invitees.map((invitee, index) => (
+            <Member
+              key={index}
+              name={invitee}
+              handleClick={() => removeInvitee(index)}
+            />
+          ))}
         </ul>
         <Button
+          type='submit'
           bgColor='bg-teal-300 hover:bg-teal-100'
           borderRadius='rounded-none'
           fontSize='text-sm'
