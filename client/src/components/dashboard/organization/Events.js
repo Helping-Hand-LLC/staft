65eb3dfcc81f1aeb3ae444f3ce1c62f51d6f650d
Skye Brown <skye.brownh@gmail.com>
2020-06-29 13:01:18 -0400

    render events in org dashboard

--- client/src/components/dashboard/organization/Events.js
+++ client/src/components/dashboard/organization/Events.js
@@ -2,6 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import DHeader from '../index/DHeader';
+import EventCard from '../../EventCard';
+import Badge from '../../../lib/Badge';
+
+import _events from '../../../constants/events.json';
 
 export default function OrgEvents({ handleClick }) {
   return (
@@ -11,8 +15,23 @@ export default function OrgEvents({ handleClick }) {
         subtitle='helpinghandllc'
         handleClick={handleClick}
       />
-      <div className='h-64 flex justify-center items-center text-gray-600'>
-        Org Events: Coming Soon...
+      <div className='px-3 py-4'>
+        {_events.map(event => (
+          <EventCard
+            key={event.id}
+            badge={
+              <Badge
+                type={event.isPublished ? 'success' : 'danger'}
+                text={event.isPublished ? 'Published' : 'Draft'}
+              />
+            }
+            location={event.location}
+            title={event.title}
+            creator={event.createdBy}
+            startDate={event.startDate}
+            startTime={event.startTime}
+          />
+        ))}
       </div>
     </>
   );
