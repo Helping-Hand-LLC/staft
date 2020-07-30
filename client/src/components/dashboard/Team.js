e625e2d74ad0e9ec9ed419bafb8c1b636f3fa568
Skye Brown <skye.brownh@gmail.com>
2020-07-30 15:02:24 -0400

    connect Team

--- client/src/components/dashboard/Team.js
+++ client/src/components/dashboard/Team.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import api from '../../utils/api';
+import * as ApiRoutes from '../../constants/ApiRoutes';
+import { useSelector } from 'react-redux';
+
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
+import Spinner from '../../lib/Spinner';
 import DashboardHeader from './DashboardHeader';
-
-import _workers from '../../constants/workers.json';
 
 function Member({ name }) {
   return (
@@ -16,40 +19,74 @@ function Member({ name }) {
 }
 
 export default function Team({ handleClick }) {
-  return (
+  const org = useSelector(state => state.org);
+
+  const [loading, setLoading] = useState(false);
+  const [allOrgWorkers, setAllOrgWorkers] = useState([]);
+
+  useEffect(() => {
+    async function fetchOrgWorkers() {
+      setLoading(true);
+      try {
+        const res = await api.get(
+          ApiRoutes.convertApiPath(ApiRoutes.GET_ORG_WORKERS, org.myOrg._id)
+        );
+        setAllOrgWorkers(res.data.orgUsers);
+        setLoading(false);
+      } catch (err) {
+        setLoading(false);
+      }
+    }
+
+    if (!org.myOrg) return;
+
+    fetchOrgWorkers();
+  }, [org.myOrg]);
+
+  return !org.myOrg ? (
     <div className='pt-16'>
-      <DashboardHeader
-        title='Team'
-        subtitle='helpinghandllc'
-        handleClick={handleClick}
-      />
-      <section className='lg:w-4/5 lg:mx-auto'>
-        <div>
-          <h3 className='font-semibold p-2 text-sm lg:text-base'>Admins</h3>
-          {_workers
-            .filter(m => m.isAdmin)
-            .map((m, i) => (
-              <Member name={m.name} key={i} />
-            ))}
-        </div>
-        <div>
-          <h3 className='font-semibold p-2 text-sm lg:text-base'>Managers</h3>
-          {_workers
-            .filter(m => !m.isAdmin && m.isManager)
-            .map((m, i) => (
-              <Member name={m.name} key={i} />
-            ))}
-        </div>
-        <div>
-          <h3 className='font-semibold p-2 text-sm lg:text-base'>Workers</h3>
-          {_workers
-            .filter(m => !m.isAdmin && !m.isManager)
-            .map((m, i) => (
-              <Member name={m.name} key={i} />
-            ))}
-        </div>
-      </section>
+      <DashboardHeader title='Team' handleClick={handleClick} />
+      <div className='h-64 flex justify-center items-center text-gray-600'>
+        Team: Join an organization to view your team
+      </div>
     </div>
+  ) : (
+    <>
+      <Spinner show={loading} />
+      <div className='pt-16'>
+        <DashboardHeader
+          title='Team'
+          subtitle={org.myOrg.uid}
+          handleClick={handleClick}
+        />
+        <section className='lg:w-4/5 lg:mx-auto'>
+          <div>
+            <h3 className='font-semibold p-2 text-sm lg:text-base'>Admins</h3>
+            {allOrgWorkers
+              .filter(m => m.isAdmin)
+              .map((m, i) => (
+                <Member name={m.name} key={i} />
+              ))}
+          </div>
+          <div>
+            <h3 className='font-semibold p-2 text-sm lg:text-base'>Managers</h3>
+            {allOrgWorkers
+              .filter(m => !m.isAdmin && m.isManager)
+              .map((m, i) => (
+                <Member name={m.name} key={i} />
+              ))}
+          </div>
+          <div>
+            <h3 className='font-semibold p-2 text-sm lg:text-base'>Workers</h3>
+            {allOrgWorkers
+              .filter(m => !m.isAdmin && !m.isManager)
+              .map((m, i) => (
+                <Member name={m.name} key={i} />
+              ))}
+          </div>
+        </section>
+      </div>
+    </>
   );
 }
 
