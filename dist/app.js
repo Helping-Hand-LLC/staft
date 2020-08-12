06cbfa9c26373b2e4b2e7ec0a29e4c3adfe02be5
Skye Brown <skye.brownh@gmail.com>
2020-08-12 12:48:48 -0400

    rebuild production code

--- dist/app.js
+++ dist/app.js
@@ -6,6 +6,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const passport_1 = __importDefault(require("passport"));
 const db_1 = __importDefault(require("./config/db"));
+const path_1 = __importDefault(require("path"));
+const keys_1 = require("./config/keys");
 const access_1 = require("./middleware/access");
 const auth_1 = __importDefault(require("./routes/auth"));
 const user_1 = __importDefault(require("./routes/user"));
@@ -19,4 +21,8 @@ app.use(passport_1.default.initialize());
 app.use('/auth', auth_1.default);
 app.use('/user', access_1.jwtAuth, user_1.default);
 app.use('/org', organization_1.default);
+if (keys_1.nodeEnv === 'production') {
+    app.use(express_1.default.static('client/build'));
+    app.get('*', (_req, res) => res.sendFile(path_1.default.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 exports.default = app;
